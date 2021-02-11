16cabe1946e0814a11b8a559bd64db36e6072235
Sandra Veraza <sandra211324@gmail.com>
2021-02-10 20:37:17 -0400

    Casi

--- index.php
+++ index.php
@@ -47,7 +47,8 @@
                
                 </ul>
                 <form class="form-inline my-2 my-lg-0">
-                    <?php 
+                    <spam class="form-control mr-sm-2" > <?php echo date('Y-m-d H:i:s');?></spam>
+                    <?php
                         if (isset($_SESSION["usuario"])) {
                             echo ' <a href="index.php?action=logout" class="btn btn-danger my-2 my-sm-0" type="submit">Cerrar Sesion</a>';
                            
@@ -75,6 +76,14 @@
         <!-- JavaScript Bundle with Popper -->
         <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
+        <script>
+            var start = new Date;
+
+            setInterval(function() {
+                $('#timer').text((new Date - start) / 1000 + " Seconds");
+            }, 1000);
+        </script>
+    
     </body>
     </html>
 
